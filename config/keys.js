87f5b285aeda2b9ac06acf36b0fed824cb8cebb0
abhishek <abhisand100@gmail.com>
2018-05-06 23:01:25 +0530

    version 1.0.0alpha done

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
-module.exports = {
-    mongoURI : 'mongodb://Abhishek:AbhiAliean1234@ds161459.mlab.com:61459/abhibooks',
-    googleClientID: '326272142801-9096hufoq458h400t9o7afov8cje0bc4.apps.googleusercontent.com',
-    googleClientSecret : 'jrpNu4D_FFx7QZxO8rA_Kr4i'
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('./keys_prod');
 
+
+}else {
+   module.exports = require('./keys_dev');
 }
---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI : 'mongodb://Abhishek:AbhiAliean1234@ds161459.mlab.com:61459/abhibooks',
+    googleClientID: '326272142801-9096hufoq458h400t9o7afov8cje0bc4.apps.googleusercontent.com',
+    googleClientSecret : 'jrpNu4D_FFx7QZxO8rA_Kr4i'
+
+}
